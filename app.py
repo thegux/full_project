1a80871fa93b2f0d296e3867e52e57839a0bc095
Gabriel GUX <gabrielsrbezerra@gmail.com>
2020-10-25 17:12:16 -0300

    fixed auth errors 500

--- app.py
+++ app.py
@@ -5,20 +5,20 @@ from flask_cors import CORS
 from models import Candidate, Company, Job, setup_db
 from auth import AuthError, requires_auth
 
+
 def create_app(test_config=None):
     app = Flask(__name__)
-    
+
     setup_db(app)
     cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
     @app.after_request
     def after_request(response):
         response.headers.add('Access-Control-Allow-Headers',
-                            'Content-type, Authorization')
+                             'Content-type, Authorization')
         response.headers.add('Access-Control-Allow-Methods',
                              'GET, POST, PATCH, DELETE, OPTIONS')
         return response
-
 
     """
     Company's section
@@ -33,9 +33,9 @@ def create_app(test_config=None):
             'status_code': 200,
             'companies': company_response,
         })
-    
+
     @app.route('/company', methods=['POST'])
-    @requires_auth('create:companies')
+    @requires_auth('post:companies')
     def create_company(jwt):
         request_value = request.get_json()
         if request_value is None:
@@ -51,12 +51,12 @@ def create_app(test_config=None):
         company.insert()
 
         return jsonify({
-          'status_code': 200,
-          'company': company.format(),
-          'message': 'The company was successfully created',
-          'success': True,
+            'status_code': 200,
+            'company': company.format(),
+            'message': 'The company was successfully created',
+            'success': True,
         })
-    
+
     @app.route('/company/<int:company_id>', methods=['PATCH'])
     @requires_auth('update:companies')
     def update_company(jwt, company_id):
@@ -72,18 +72,18 @@ def create_app(test_config=None):
         company.update()
 
         return jsonify({
-          'status_code': 200,
-          'company': company.format(),
-          'message': 'The Company was successfully edited',
-          'success': True,
+            'status_code': 200,
+            'company': company.format(),
+            'message': 'The Company was successfully edited',
+            'success': True,
         })
-    
+
     @app.route('/company/<int:company_id>', methods=['DELETE'])
     @requires_auth('delete:companies')
     def delete_company(jwt, company_id):
         company = Company.query.filter_by(id=company_id).one_or_none()
 
-        if company is None: 
+        if company is None:
             abort(404)
 
         company.delete()
@@ -92,10 +92,6 @@ def create_app(test_config=None):
             'message': 'The Company was successfully deleted',
             'status_code': 200,
         })
-
-    
-
-
 
     """
         Job's section
@@ -112,13 +108,13 @@ def create_app(test_config=None):
         })
 
     @app.route('/job', methods=['POST'])
-    @requires_auth('create:jobs')
+    @requires_auth('post:jobs')
     def create_job(jwt):
         request_value = request.get_json()
 
         if request_value is None:
             abort(400)
- 
+
         job = Job(
             request_value['title'],
             request_value['description'],
@@ -129,16 +125,16 @@ def create_app(test_config=None):
             request_value['is_remote'],
             request_value['company_id']
         )
-        
+
         job.insert()
 
         return jsonify({
-          'status_code': 200,
-          'job': job.format_long(),
-          'message': 'The job was successfully created',
-          'success': True,
+            'status_code': 200,
+            'job': job.format_long(),
+            'message': 'The job was successfully created',
+            'success': True,
         })
-    
+
     @app.route('/job/<int:job_id>', methods=['PATCH'])
     @requires_auth('update:jobs')
     def update_job(jwt, job_id):
@@ -156,10 +152,10 @@ def create_app(test_config=None):
         job.update()
 
         return jsonify({
-          'status_code': 200,
-          'job': job.format_long(),
-          'message': 'The job was successfully edited',
-          'success': True,
+            'status_code': 200,
+            'job': job.format_long(),
+            'message': 'The job was successfully edited',
+            'success': True,
         })
 
     @app.route('/job/<int:job_id>', methods=['DELETE'])
@@ -167,7 +163,7 @@ def create_app(test_config=None):
     def delete_job(jwt, job_id):
         job = Job.query.filter_by(id=job_id).one_or_none()
 
-        if job is None: 
+        if job is None:
             abort(404)
         job.delete()
 
@@ -190,7 +186,7 @@ def create_app(test_config=None):
             'status_code': 200,
             'candidates': candidates_response,
         })
-    
+
     @app.route('/candidates/<int:job_id>')
     @requires_auth('get:job_candidates')
     def get_candidates_by_job(jwt, job_id):
@@ -202,83 +198,82 @@ def create_app(test_config=None):
             'status_code': 200,
             'candidates': candidates_response,
         })
-        
 
     @app.route('/candidate/apply', methods=['POST'])
     def apply_job():
         request_value = request.get_json()
         job_id = request_value['job_id']
         company_id = request_value['company_id']
-    
+
         job = Job.query.filter_by(id=job_id).one_or_none()
         if job is None:
             abort(404)
-        
+
         company = Company.query.filter_by(id=company_id).one_or_none()
         if company is None:
             abort(404)
-        
+
         candidate = Candidate(
-                        job_id,
-                        company_id,
-                        request_value['name'],
-                        request_value['email'],
-                        request_value['phone']
-                    )
+            job_id,
+            company_id,
+            request_value['name'],
+            request_value['email'],
+            request_value['phone']
+        )
 
         candidate.insert()
-        
+
         return jsonify({
             'message': 'Your application was successfully sent.',
             'status_code': 200,
         })
-
-    return app
-
-    '''
+        '''
     Example error handling for unprocessable entity
     '''
     @app.errorhandler(400)
     def bad_request(error):
         return jsonify({
-                        "success": False, 
-                        "error": 400,
-                        "message": "bad request"
-                        }), 400
+            "success": False,
+            "error": 400,
+            "message": "bad request"
+        }), 400
+
 
     @app.errorhandler(422)
     def unprocessable(error):
         return jsonify({
-                        "success": False, 
-                        "error": 422,
-                        "message": "unprocessable"
-                        }), 422
-
-    @app.errorhandler(404)
-    def not_found(error):
-        return jsonify({
-                        "success": False, 
-                        "error": 404,
-                        "message": "Not found."
-                        }), 404
-
-    @app.errorhandler(500)
-    def server_error(error):
-        return jsonify({
-                        "success": False, 
-                        "error": 500,
-                        "message": "Sorry, we couldn't handle your request."
-                        }), 500
+            "success": False,
+            "error": 422,
+            "message": "unprocessable"
+        }), 422
 
 
     @app.errorhandler(AuthError)
     def auth_error(error):
         print('aaaaaaaaaaaa')
         return jsonify({
-                        "success": False, 
-                        "error": error.status_code,
-                        "message": error.error['description']
-                        }), error.status_code
+            "success": False,
+            "error": error.status_code,
+            "message": error.error['description']
+        }), error.status_code
+
+
+    @app.errorhandler(404)
+    def not_found(error):
+        return jsonify({
+            "success": False,
+            "error": 404,
+            "message": "not found"
+        }), 404
+
+
+    @app.errorhandler(500)
+    def server_error(error):
+        return jsonify({
+            "success": False,
+            "error": 500,
+            "message": "Sorry, we couldn't handle your request."
+        }), 500
 
     return app
 
